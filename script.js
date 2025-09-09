f3385a462501d32e18bdad208d1710b96e80d92a
Eswara pandi <eswarapandi383@gmail.com>
2025-09-09 18:00:37 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Show or hide contact details
-function showContact() {
-  let contact = document.getElementById("contact-info");
-  if (contact.style.display === "none") {
-    contact.style.display = "block";
-  } else {
-    contact.style.display = "none";
-  }
-}
